5d111375ac1819e9d105e8563da5659365b1009e
temmali <86911439+temmali@users.noreply.github.com>
2021-07-05 13:08:03 +0300

    Update Dali.sh

--- Dali.sh
+++ Dali.sh
@@ -18,7 +18,7 @@ sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y
 if [ "$Version" == "18" ]; then
 echo -e "\033[0;31m\n~ Installing Depedencies For Ubuntu 18... \n\033[0m"
 cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
-wget "https://raw.githubusercontent.com/temmali/Devali/main/ANGEL.lua"" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
+wget "https://raw.githubusercontent.com/temmali/Devali/main/Dali.lua"" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
 rm compat-libevent2-5_2.0.21-1ubuntu18_amd64.deb
 fi
 sudo apt-get install screen -y
